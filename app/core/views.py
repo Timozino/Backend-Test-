56e7e02f5f485d5e1f1c41fbd67921a026bc561b
Timozino <mod.timson@gmail.com>
2024-07-24 12:47:09 +0100

    ADD[Implemented the logic for Category and Product using viewset]

--- app/core/views.py
+++ app/core/views.py
@@ -1,6 +1,9 @@
 from rest_framework import generics
 from rest_framework.response import Response
 from .serializers import UserSerializer, UserLoginSerializer, MyTokenObtainPairSerializer
+from rest_framework import viewsets
+from .models import Product, Category
+from .serializers import ProductSerializer, CategorySerializer
 
 class UserCreateAPIView(generics.CreateAPIView):
     serializer_class = UserSerializer
@@ -12,3 +15,15 @@ class UserLoginAPIView(generics.GenericAPIView):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         return Response(serializer.validated_data)
+    
+    
+    
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
