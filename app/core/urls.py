55d68a30439959616088ed4b7ac655f01bc00e17
Timozino <mod.timson@gmail.com>
2024-07-24 12:58:18 +0100

    ADD[Created a router config for Category and Product]

--- app/core/urls.py
+++ app/core/urls.py
@@ -1,10 +1,25 @@
 from django.urls import path
 from .views import UserCreateAPIView, UserLoginAPIView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import CategoryViewSet, ProductViewSet
+
+
+
+
+router = DefaultRouter()
+router.register(r'categories', CategoryViewSet)
+router.register(r'products', ProductViewSet)
+
+
+
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('register/', UserCreateAPIView.as_view(), name='register'),
     path('login/', UserLoginAPIView.as_view(), name='login'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+
